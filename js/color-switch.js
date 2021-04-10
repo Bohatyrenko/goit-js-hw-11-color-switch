306c8135e8aed61d529e031f62a84dd71ec95d74
Oleksii Bohatyrenko <steenbras31@gmail.com>
2021-04-10 22:40:32 +0300

    Добавляет переключатель цветов и стили

--- js/color-switch.js
+++ js/color-switch.js
@@ -7,6 +7,23 @@ const colors = [
   "#795548",
 ];
 
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+let intervalId = null;
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+startBtn.addEventListener("click", () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+});
